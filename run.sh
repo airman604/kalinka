110b2b76a3621c70b15bdf7aeeb00540610d6fd7
airman604 <4887927+airman604@users.noreply.github.com>
2020-09-13 10:08:21 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # ssh-keygen -t ed25519
+# cp ~/.ssh/id_ed25519.pub ~/.ssh/authorized_keys
 sudo apt install -y ansible
 sudo systemctl start ssh
-ansible-playbook -i inventory.ini -k -K playbook.yml
+ANSIBLE_HOST_KEY_CHECKING=True ansible-playbook -i inventory.ini -k -K playbook.yml
 # -k - ask SSH password
 # -K - ask sudo password
 sudo systemctl stop ssh
