49c790c79180d3f7759bf0986153c16e9008d68b
airman604 <4887927+airman604@users.noreply.github.com>
2020-09-13 11:46:59 -0700

    fixed supertty.yml

--- roles/more-tools/templates/supertty.fish
+++ roles/more-tools/templates/supertty.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function supertty --wraps='docker run --rm -it airman604/supertty' --description 'alias supertty docker run --rm -it airman604/supertty'
-  docker run --rm -it airman604/supertty $argv;
+function supertty --wraps='docker run --rm -it supertty' --description 'alias supertty docker run --rm -it supertty'
+  docker run --rm -it supertty $argv;
 end
