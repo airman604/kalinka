8aef7196266e89cc41b0e856f0504320f64d208d
airman604 <4887927+airman604@users.noreply.github.com>
2020-09-13 11:41:31 -0700

    make ansible run against localhost without ssh

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# ssh-keygen -t ed25519
-# cp ~/.ssh/id_ed25519.pub ~/.ssh/authorized_keys
 sudo apt install -y ansible
-sudo systemctl start ssh
-ANSIBLE_HOST_KEY_CHECKING=True ansible-playbook -i inventory.ini -k -K playbook.yml
-# -k - ask SSH password
 # -K - ask sudo password
-sudo systemctl stop ssh
+ansible-playbook -i inventory.ini -k -K playbook.yml
